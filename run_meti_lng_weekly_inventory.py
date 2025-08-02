98902b882df32e4a89b7b5be7085817af39292cb
dlxiii <tokyowangyulong@gmail.com>
2025-08-03 03:02:13 +0900

    Add robust retry logic for METI LNG stock download

--- run_meti_lng_weekly_inventory.py
+++ run_meti_lng_weekly_inventory.py
@@ -4,13 +4,18 @@ import sys
 from meti_scraper import meti
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print('Usage: python run_meti_lng_weekly_inventory.py YYYY/MM/DD')
+        print("Usage: python run_meti_lng_weekly_inventory.py YYYY/MM/DD")
         sys.exit(1)
 
     date_str = sys.argv[1]
-    dt = datetime.strptime(date_str, '%Y/%m/%d')
+    dt = datetime.strptime(date_str, "%Y/%m/%d")
 
     scraper = meti()
-    scraper.lng_weekly_inventory(date=dt.strftime('%Y%m%d'))
+    try:
+        scraper.lng_weekly_inventory(date=dt.strftime("%Y%m%d"))
+    except RuntimeError as err:
+        print(err)
+        sys.exit(1)
+
