ceeffe04be32d4010631e9b489496d2f12b12307
dlxiii <opendenki@gmail.com>
2025-08-03 02:53:10 +0900

    Calculate previous Wednesday automatically and use it to download METI LNG weekly stock PDF Remove command-line argument requirement in entry scripts

--- run_meti_lng_weekly_inventory.py
+++ run_meti_lng_weekly_inventory.py
@@ -1,12 +1,16 @@
-from datetime import datetime, timedelta
+from datetime import datetime
+import sys
 
 from meti_scraper import meti
 
 
 if __name__ == '__main__':
-    today = datetime.today()
-    offset = (today.weekday() - 2) % 7 or 7
-    last_wednesday = today - timedelta(days=offset)
+    if len(sys.argv) != 2:
+        print('Usage: python run_meti_lng_weekly_inventory.py YYYY/MM/DD')
+        sys.exit(1)
+
+    date_str = sys.argv[1]
+    dt = datetime.strptime(date_str, '%Y/%m/%d')
 
     scraper = meti()
-    scraper.lng_weekly_inventory(date=last_wednesday.strftime('%Y%m%d'))
+    scraper.lng_weekly_inventory(date=dt.strftime('%Y%m%d'))
